9b2b24dc4b7d5dff64ea5dce55178bfdf77932f3
Ardaerd <arderd@outlook.com>
2022-12-26 11:19:24 +0300

    ServiceController is implemented

--- src/main/java/com/example/RentCar/Controller/ServiceController.java
+++ src/main/java/com/example/RentCar/Controller/ServiceController.java
@@ -1,5 +1,46 @@
 package com.example.RentCar.Controller;
 
 
+import com.example.RentCar.DTO.ServiceDTO;
+import com.example.RentCar.Service.ServiceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/Services")
+@CrossOrigin
 public class ServiceController {
+
+    @Autowired
+    ServiceService serviceService;
+
+    @GetMapping(value = "/getServices")
+    @Operation(summary = "Find all services", description = "Get all the services")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = ServiceDTO.class))),
+            @ApiResponse(responseCode = "404", description = "There is no service") })
+    public ResponseEntity<List<ServiceDTO>> getAllServices() {
+        List<ServiceDTO> dtoList = serviceService.getAllServiceList();
+        return ResponseEntity.status(HttpStatus.OK).body(dtoList);
+    }
+
+    @GetMapping(value = "/getServices/{reservationNum}/{serviceCode}")
+    @Operation(summary = "Add service to the reservation", description = "Add service to the reservation")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = ServiceDTO.class))),
+            @ApiResponse(responseCode = "404", description = "Not found"),
+            @ApiResponse(responseCode = "505", description = "exception thrown")})
+    public ResponseEntity<Boolean> addAdditionalServicesToReservation(@PathVariable("reservationNum") String reservationNumber, @PathVariable("serviceCode") int serviceCode) {
+        Boolean isAdded = serviceService.addAdditionalServiceToReservation(reservationNumber,serviceCode);
+        return ResponseEntity.status(HttpStatus.OK).body(isAdded);
+    }
 }
